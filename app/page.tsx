954bb28a01effb5b82a3e6eca2ff9afe9ebd3ffd
N.S.Devaraj <nsdevaraj@gmail.com>
2024-08-26 21:33:18 +0530

    prompt onboarding

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+"use client";
+import UserOnboarding from '../components/user-onboarding';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>empty page</div>
+      <UserOnboarding />
     </main>
   );
 }
